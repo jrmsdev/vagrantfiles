c4124cc315ffa6bfe9f4a19b9838bb9e46b87ee3
Jeremías <jrmsdev@gmail.com>
2017-11-04 23:05:13 -0300

    fix bug

--- scripts/vagrant-sshauth.sh
+++ scripts/vagrant-sshauth.sh
@@ -2,7 +2,7 @@
 
 PUBKEY_URL=https://raw.github.com/hashicorp/vagrant/master/keys/vagrant.pub
 
-if test 'FreeBSD' == $(uname -s); then
+if test 'FreeBSD' = $(uname -s); then
     fetch -o ~vagrant/.ssh/authorized_keys ${PUBKEY_URL}
 else
     wget -q -O ~vagrant/.ssh/authorized_keys ${PUBKEY_URL}
